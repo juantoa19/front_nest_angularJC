0e825343ddb70125c6390c6265e1c99c3558f143
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-05-31 09:02:25 -0500

    Configuracion Login

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
     ClienteComponent,
-    PerfilComponent
+    PerfilComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule,
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  private http = inject(HttpClient)
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.email, Validators.required]),  ////Valores por defecto
+    password: new FormControl("", Validators.required)    ////Valores por defecto
+  })
+
+  funIngresar() {
+    this.http.get("http://jsonplaceholder.typicode.com/todos").subscribe(
+      (res)=>{
+        console.log(res)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+    ////alert("Ingresando...")
+  }
 }
