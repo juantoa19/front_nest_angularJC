2859fe2cc43fd7a89f0e8ba91d119e7f6eaaa9e9
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-06-03 18:24:30 -0500

    Solucionado

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  
-  private baseUrl= "http://127.0.0.1:3000";
-  
-  constructor(private http: HttpClient) {}
+
+  private baseUrl = "http://127.0.0.1:3000";
+
+  constructor(private http: HttpClient) { }
 
   loginConNest(credenciales: any) {
-    return this.http.get<any>(`${this.baseUrl}/auth/login`, credenciales);
+    return this.http.post<any>(`${this.baseUrl}/auth/login`, credenciales);
   }
 
   registroConNest(datos: any) {
